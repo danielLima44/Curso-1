415e2741ea642717c1d05d11dc3b3c48167d6311
DANIEL DOS SANTOS LIMA <135070260+danielLima44@users.noreply.github.com>
2023-06-01 21:39:06 -0300

    2-aula
    Explicação é adendos!

--- App.js
+++ App.js
@@ -1,26 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
  export default class App extends React.Component{
     render(){
         return(
             <View style = {StyleSheet.container}>
                 <Text>Hello Git!</Text>
-                <StatusBar style = "auto"/> 
+               <Image style={{width:50,height:100}} 
+               source= {{uri:'Link da image'}}
+               />
                 </View>
         );
     }
 }
-      const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-  }
-)
+    
+   
+  
+
 //backgroundColor= Cor de fundo!
 //alignItems= Alinhar os itens!
 //JustifyContent= Alinhar os itens
@@ -28,5 +25,7 @@ import { StyleSheet, Text, View } from 'react-native';
 // <View Sytle....> <Text> OLá </Text> </View> Obs; Sempre Re-Ler 
 // Para não esquecer a letra se estáa maiuscula ou minuscula, Tem que está igual ao Import!
 // return= utilizar sempre depois do rende é antes do view ´w exemplos como header, body, footer usar logo abaixo!
- // render= tudo vai sair no app!
-
+// render= tudo vai sair no app!
+// Image= Modificar o estilo, Tal como tamhanho é espessura!Usar style!
+// Source= utilizar para adicionar link com o uri dentre outras coisas,
+// Usar{{}} (Sempre que for usar link de imagem usar uri:"***")
